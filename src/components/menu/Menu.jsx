c7352caeff123097e493a01271e7ec6729ecc6e3
Harnish Mistry <harrnish@gmail.com>
2024-03-06 17:06:51 -0500

    menu: functional

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,51 +1,57 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import "./menu.css";
+import { gsap } from "gsap";
+
 import { Link } from "react-router-dom";
 
-import "./menu.css";
-
 const Menu = () => {
+  const menuContainer = useRef();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuAnimation = useRef();
+
+  const menuLinks = [
+    { path: "/", label: "Home" },
+    { path: "/about", label: "About" },
+    { path: "/services", label: "Services" },
+    { path: "/works", label: "Work" },
+    { path: "/contact", label: "Contact" },
+    { path: "/careers", label: "Careers" },
+  ];
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  useEffect(() => {
+    menuAnimation.current = gsap.timeline({ paused: true }).to(".menu", {
+      duration: 1,
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      ease: "power4.inOut",
+    });
+  }, []);
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      menuAnimation.current.play();
+    } else {
+      menuAnimation.current.reverse();
+    }
+  }, [isMenuOpen]);
+
   return (
-    <div className="menu">
-      <div className="menu-logo">
-        <Link className="menu-link" to="/">
-          Cura Futuri
-        </Link>
+    <div className="menu-container" ref={menuContainer}>
+      <div className="menu-toggle">
+        <button onClick={() => setIsMenuOpen(!isMenuOpen)}>Menu</button>
       </div>
-      <div className="menu-links">
-        <div className="menu-link-item">
-          <Link className="menu-link" to="/">
-            Home
-          </Link>
-        </div>
-
-        <div className="menu-link-item">
-          <Link className="menu-link" to="/about">
-            About
-          </Link>
-        </div>
-
-        <div className="menu-link-item">
-          <Link className="menu-link" to="/services">
-            Services
-          </Link>
-        </div>
-
-        <div className="menu-link-item">
-          <Link className="menu-link" to="/works">
-            Work
-          </Link>
-        </div>
-
-        <div className="menu-link-item">
-          <Link className="menu-link" to="/contact">
-            Contact
-          </Link>
-        </div>
-
-        <div className="menu-link-item">
-          <Link className="menu-link" to="/careers">
-            Careers
-          </Link>
+      <div className="menu">
+        <div className="menu-links">
+          {menuLinks.map((link, index) => (
+            <div key={index} className="menu-link-item" onClick={toggleMenu}>
+              <Link className="menu-link" to={link.path}>
+                {link.label}
+              </Link>
+            </div>
+          ))}
         </div>
       </div>
     </div>
