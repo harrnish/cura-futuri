27d0674e7e15e4a64fcaaa7a4f5429025617e32f
Harnish Mistry <harrnish@gmail.com>
2024-03-07 22:32:48 -0500

    hero section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,10 +3,24 @@ import Transition from "../../components/transition/Transition";
 
 import "./home.css";
 
+import HeroImage from "../../assets/images/home/hero.jpg";
+
 const Home = () => {
   return (
     <div className="container">
-      <h1>Home</h1>
+      <div className="hero-section">
+        <div className="hero-copy">
+          <h2>A new type of marketing agency</h2>
+        </div>
+
+        <div className="hero-img">
+          <img src={HeroImage} alt="" />
+        </div>
+
+        <div className="hero-header">
+          <h1>2.0</h1>
+        </div>
+      </div>
     </div>
   );
 };
