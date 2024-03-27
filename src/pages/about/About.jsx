bbb399802a60a7fb6832abdc4ff301f2fa99b380
Harnish Mistry <harrnish@gmail.com>
2024-03-27 15:37:32 -0400

    about page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -5,8 +5,19 @@ import "./about.css";
 
 const About = () => {
   return (
-    <div className="container">
-      <h1>About</h1>
+    <div className="about page">
+      <div className="container">
+        <section className="about-marquee">
+          <div className="about-row">
+            <div className="about-col">
+              <p>1</p>
+            </div>
+            <div className="about-col">
+              <p>2</p>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
