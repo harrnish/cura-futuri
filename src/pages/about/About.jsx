beefc39d4e52eda5131a02fc79cfffaa2774bd25
Harnish Mistry <harrnish@gmail.com>
2024-03-28 14:38:44 -0400

    projects page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,22 +1,305 @@
 import React from "react";
 import Transition from "../../components/transition/Transition";
 
+import Faq from "react-faq-component";
+
+import Marquee from "react-fast-marquee";
+
+import MagneticButton from "../../components/magneticbutton/MagneticButton";
+
 import "./about.css";
 
 const About = () => {
+  const data = {
+    title: "",
+    rows: [
+      {
+        title: "What type of works do you take on?",
+        content:
+          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate, perspiciatis fugiat inventore excepturi vel voluptas, doloremque architecto quidem, optio autem at maxime labore possimus accusamus ab deleniti deserunt harum quasi cumque odio? Reiciendis eligendi tempore eum distinctio delectus aspernatur? Consequuntur! ",
+      },
+      {
+        title: "How do you charge for projects?",
+        content:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis sit molestiae fugit ipsum commodi at aspernatur laudantium natus qui dignissimos facilis, libero consectetur culpa sunt? Atque nobis ut quam vel.",
+      },
+      {
+        title: "What is your hourly rate?",
+        content:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea vel iure eius, error consequuntur quae quibusdam iusto ducimus temporibus quod, itaque saepe rerum distinctio dolor omnis culpa fugiat. Ad, est temporibus omnis ullam necessitatibus a!",
+      },
+    ],
+  };
   return (
     <div className="about page">
       <div className="container">
         <section className="about-marquee">
+          <Marquee>
+            <h1>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae,
+              odit?
+            </h1>
+          </Marquee>
+        </section>
+
+        <section className="about-intro">
+          <h2>
+            Naomi is an award-winning Designer, currently working at Fouroom in
+            London and specializing in brand identities, editorial design and
+            typography. She works for a variety of clients in Fashion, Beauty,
+            Culture and Tech, she creates design for brands of all shapes and
+            sizes through Branding, Illustration and Art Direction.
+          </h2>
+        </section>
+
+        <section className="about-intro-copy">
           <div className="about-row">
             <div className="about-col">
-              <p>1</p>
+              <p>
+                <span>Est. 1990</span>
+              </p>
             </div>
             <div className="about-col">
-              <p>2</p>
+              <h3>
+                She works for a variety of clients in Fashion, Beauty, Culture
+                and Tech, she creates design for brands of all shapes and sizes
+                through Branding, Illustration and Art Direction. Originally
+                from Finland, Naomi Wilson studied graphic design and
+                illustration at Hyper Island in Stockholm.
+              </h3>
+              <h3 style={{ textIndent: "100px" }}>
+                She has been nominated for mi quis viverra ornare, eros dolor
+                interdum nulla, ut commodo diam libero vitae erat, and has given
+                talks at nunc ut sem vitae risus tristique posuere across the
+                United States and Europe.
+              </h3>
             </div>
           </div>
         </section>
+
+        <section className="about-portrait">
+          <div className="about-row">
+            <div className="about-col">
+              <p>
+                <span>Contact</span>
+              </p>
+              <br />
+              <br />
+              <p>
+                <span>Email : </span> <span>hello@studio.com</span>
+              </p>
+              <p>
+                <span>Phone : </span> <span>+44 182 304 5922</span>
+              </p>
+              <p>
+                <span>Address : </span> <span>222-3333 Dummy Road</span>
+              </p>
+              <br />
+              <br />
+              <br />
+            </div>
+            <div className="about-col">
+              <div className="about-portrait-img">
+                <img
+                  src="https://assets-global.website-files.com/61385c793ab59f1f6a3372f7/6138b0c979627a47553f5f11_pexels-9431879.jpg"
+                  alt=""
+                />
+              </div>
+
+              <div className="faqs">
+                <Faq data={data} />
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="about-awards">
+          <div className="about-row">
+            <div className="about-col"></div>
+            <div className="about-col award-header">
+              <p>
+                <span>Honors and Awards</span>
+              </p>
+            </div>
+          </div>
+
+          <div className="about-row award-row">
+            <div className="about-col">
+              <div className="award-year">
+                <p>2021</p>
+              </div>
+              <div className="award-view">
+                <p>Featured on Website</p>
+              </div>
+            </div>
+            <div className="about-col">
+              <div className="award-info">
+                <p>Awwwards</p>
+              </div>
+              <div className="award-project">
+                <p>Your project</p>
+              </div>
+            </div>
+          </div>
+          <div className="about-row award-row">
+            <div className="about-col">
+              <div className="award-year">
+                <p>2021</p>
+              </div>
+              <div className="award-view">
+                <p>Featured on Website</p>
+              </div>
+            </div>
+            <div className="about-col">
+              <div className="award-info">
+                <p>Awwwards</p>
+              </div>
+              <div className="award-project">
+                <p>Your project</p>
+              </div>
+            </div>
+          </div>
+          <div className="about-row award-row">
+            <div className="about-col">
+              <div className="award-year">
+                <p>2021</p>
+              </div>
+              <div className="award-view">
+                <p>Featured on Website</p>
+              </div>
+            </div>
+            <div className="about-col">
+              <div className="award-info">
+                <p>Awwwards</p>
+              </div>
+              <div className="award-project">
+                <p>Your project</p>
+              </div>
+            </div>
+          </div>
+          <div className="about-row award-row">
+            <div className="about-col">
+              <div className="award-year">
+                <p>2021</p>
+              </div>
+              <div className="award-view">
+                <p>Featured on Website</p>
+              </div>
+            </div>
+            <div className="about-col">
+              <div className="award-info">
+                <p>Awwwards</p>
+              </div>
+              <div className="award-project">
+                <p>Your project</p>
+              </div>
+            </div>
+          </div>
+          <div className="about-row award-row">
+            <div className="about-col">
+              <div className="award-year">
+                <p>2021</p>
+              </div>
+              <div className="award-view">
+                <p>Featured on Website</p>
+              </div>
+            </div>
+            <div className="about-col">
+              <div className="award-info">
+                <p>Awwwards</p>
+              </div>
+              <div className="award-project">
+                <p>Your project</p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="about-awards">
+          <div className="about-row">
+            <div className="about-col"></div>
+            <div className="about-col award-header">
+              <p>
+                <span>In Press</span>
+              </p>
+            </div>
+          </div>
+
+          <div className="about-row award-row">
+            <div className="about-col">
+              <div className="award-year">
+                <p>2021</p>
+              </div>
+              <div className="award-view">
+                <p>Featured on Website</p>
+              </div>
+            </div>
+            <div className="about-col">
+              <div className="award-info">
+                <p>Awwwards</p>
+              </div>
+              <div className="award-project">
+                <p>Your project</p>
+              </div>
+            </div>
+          </div>
+          <div className="about-row award-row">
+            <div className="about-col">
+              <div className="award-year">
+                <p>2021</p>
+              </div>
+              <div className="award-view">
+                <p>Featured on Website</p>
+              </div>
+            </div>
+            <div className="about-col">
+              <div className="award-info">
+                <p>Awwwards</p>
+              </div>
+              <div className="award-project">
+                <p>Your project</p>
+              </div>
+            </div>
+          </div>
+          <div className="about-row award-row">
+            <div className="about-col">
+              <div className="award-year">
+                <p>2021</p>
+              </div>
+              <div className="award-view">
+                <p>Featured on Website</p>
+              </div>
+            </div>
+            <div className="about-col">
+              <div className="award-info">
+                <p>Awwwards</p>
+              </div>
+              <div className="award-project">
+                <p>Your project</p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="about-contact">
+          <div className="about-contact-copy">
+            <h2>Powered by Webflow.com</h2>
+            <p>
+              <span>
+                For a CV and inquiries please reach out to hello@template.co
+              </span>
+            </p>
+
+            <br />
+            <p>
+              <span>+44 182 304 5923</span>
+            </p>
+            <p>
+              <span>Instagram • Twitter</span>
+            </p>
+          </div>
+        </section>
+        <MagneticButton />
       </div>
     </div>
   );
--- src/pages/works/Works.jsx
+++ src/pages/works/Works.jsx
@@ -1,12 +1,210 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import Transition from "../../components/transition/Transition";
+import { Link } from "react-router-dom";
+
+import { gsap } from "gsap";
+
+import Marquee from "react-fast-marquee";
+import MagneticButton from "../../components/magneticbutton/MagneticButton";
 
 import "./works.css";
 
+import ProjectImg1 from "../../assets/images/projects/project-1.jpg";
+import ProjectImg2 from "../../assets/images/projects/project-2.jpg";
+import ProjectImg3 from "../../assets/images/projects/project-3.jpg";
+import ProjectImg4 from "../../assets/images/projects/project-4.jpg";
+import ProjectImg5 from "../../assets/images/projects/project-5.jpg";
+
 const Works = () => {
+  const projectPreivewImages = [
+    ProjectImg1,
+    ProjectImg2,
+    ProjectImg3,
+    ProjectImg4,
+    ProjectImg5,
+  ];
+
+  let lastHoveredIndex = null;
+  const projectPreviewContainer = document.querySelector(".project-preview");
+
+  const handleResetPreivew = () => {
+    const projectPreviewContainer = document.querySelector(".project-preview");
+
+    gsap.to(".project-preview img", {
+      opacity: 0,
+      duration: 0.3,
+      onComplete: () => {
+        lastHoveredIndex = -1;
+      },
+    });
+  };
+
+  const handleMouseOver = (index) => {
+    const projectPreviewContainer = document.querySelector(".project-preview");
+
+    if (index !== lastHoveredIndex) {
+      console.log(`Hovered ${index}`);
+
+      const img = document.createElement("img");
+      img.src = projectPreivewImages[index - 1];
+      projectPreviewContainer.appendChild(img);
+
+      gsap.to(img, {
+        opacity: 1,
+        duration: 0.3,
+        onComplete: () => {
+          const allPrevImages = projectPreviewContainer.querySelectorAll("img");
+
+          if (allPrevImages.length > 1) {
+            Array.from(allPrevImages)
+              .slice(0, -1)
+              .forEach((img) => {
+                setTimeout(() => {
+                  img.remove();
+                }, 1000);
+              });
+          }
+        },
+      });
+
+      lastHoveredIndex = index;
+    }
+  };
+
+  useEffect(() => {
+    const projectItems = document.querySelectorAll(".project-item");
+
+    return () => {
+      projectItems.forEach((projectItem) => {
+        projectItem.removeEventListener("mouseover", () =>
+          handleMouseOver(index)
+        );
+      });
+    };
+  }, []);
+
   return (
-    <div className="container">
-      <h1>Works</h1>
+    <div className="works page">
+      <div className="project-preview"></div>
+      <div className="container">
+        <section
+          className="works-hero"
+          onMouseOver={() => {
+            handleResetPreivew();
+          }}
+        >
+          <h1>CASES</h1>
+          <h1>2018 • 2021</h1>
+        </section>
+
+        <section className="project-list">
+          <div className="project-list-row">
+            <div className="project-list-col">
+              <div
+                className="project-item"
+                onMouseOver={() => handleMouseOver(1)}
+              >
+                <div className="project-img">
+                  <Link to="/">
+                    <img src={ProjectImg1} alt="" />
+                  </Link>
+                </div>
+                <div className="project-copy copy-pos-right">
+                  <h2>Project Name 1</h2>
+                </div>
+              </div>
+            </div>
+            <div className="project-list-col whitespace-col"></div>
+          </div>
+
+          <div className="project-list-row">
+            <div className="project-list-col whitespace-col"></div>
+            <div className="project-list-col">
+              <div
+                className="project-item"
+                onMouseOver={() => handleMouseOver(2)}
+              >
+                <div className="project-img">
+                  <Link to="/">
+                    <img src={ProjectImg2} alt="" />
+                  </Link>
+                </div>
+                <div className="project-copy copy-pos-left">
+                  <h2>Project Name 2</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="project-list-row">
+            <div className="project-list-col">
+              <div
+                className="project-item"
+                onMouseOver={() => handleMouseOver(3)}
+              >
+                <div className="project-img">
+                  <Link to="/">
+                    <img src={ProjectImg3} alt="" />
+                  </Link>
+                </div>
+                <div className="project-copy copy-pos-right">
+                  <h2>Project Name 3</h2>
+                </div>
+              </div>
+            </div>
+            <div className="project-list-col whitespace-col"></div>
+          </div>
+
+          <div className="project-list-row">
+            <div className="project-list-col whitespace-col"></div>
+            <div className="project-list-col">
+              <div
+                className="project-item"
+                onMouseOver={() => handleMouseOver(4)}
+              >
+                <div className="project-img">
+                  <Link to="/">
+                    <img src={ProjectImg4} alt="" />
+                  </Link>
+                </div>
+                <div className="project-copy copy-pos-left">
+                  <h2>Project Name 4</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="project-list-row">
+            <div className="project-list-col">
+              <div
+                className="project-item"
+                onMouseOver={() => handleMouseOver(5)}
+              >
+                <div className="project-img">
+                  <Link to="/">
+                    <img src={ProjectImg5} alt="" />
+                  </Link>
+                </div>
+                <div className="project-copy copy-pos-right">
+                  <h2>Project Name 5</h2>
+                </div>
+              </div>
+            </div>
+            <div className="project-list-col whitespace-col"></div>
+          </div>
+        </section>
+
+        <div className="works-marquee">
+          <Marquee>
+            <h1>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae,
+              odit?
+            </h1>
+          </Marquee>
+        </div>
+
+        <MagneticButton />
+      </div>
     </div>
   );
 };
